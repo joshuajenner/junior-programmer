2ed3a0f120cb638914014edccf273379770280ff
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-12-04 17:20:11 +0800

    completed 4.4

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9f;
 
-    // Start is called before the first frame update
+    public int enemyCount;
+    public int waveNumber = 0;
+
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup();
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup();
+        }
+    }
+    private void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -21,9 +40,12 @@ public class SpawnManager : MonoBehaviour
         return new Vector3(spawnPosX, 0, spawnPosZ);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemyWave(int enemiesToSpawn)
     {
-        
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
+ 
