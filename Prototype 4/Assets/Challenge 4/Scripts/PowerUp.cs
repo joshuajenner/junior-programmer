d1c236b309eb824fab55bd20a4467ee3b887c1fe
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-12-08 17:58:23 +0800

    Update PowerUp.cs

--- Prototype 4/Assets/Challenge 4/Scripts/PowerUp.cs
+++ Prototype 4/Assets/Challenge 4/Scripts/PowerUp.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PowerUpType { None, Pushback, Rockets }
+
 public class PowerUp : MonoBehaviour
 {
+
+    public PowerUpType powerUpType;
+
+
     // Start is called before the first frame update
     void Start()
     {
