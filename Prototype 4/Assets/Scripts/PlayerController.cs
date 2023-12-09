7ad3ea30a75781c43c778167162f40fb10ccae77
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-12-10 01:08:55 +0800

    added smash powerup

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     public GameObject bumpPowerupIndicator;
     public GameObject shootPowerupIndicator;
+    public GameObject smashPowerupIndicator;
 
     public float speed = 5.0f;
     private bool hasPowerup = false;
@@ -18,6 +19,15 @@ public class PlayerController : MonoBehaviour
     public GameObject rocketPrefab;
     private GameObject tmpRocket;
     private Coroutine powerupCountdown;
+
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+
+    bool smashing = false;
+    float floorY;
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,10 +43,17 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
         bumpPowerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
         shootPowerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        smashPowerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
 
         if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
         {
             LaunchRockets();
+        }
+
+        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            smashing = true;
+            StartCoroutine(Smash());
         }
 
     }
@@ -48,7 +65,20 @@ public class PlayerController : MonoBehaviour
             hasPowerup = true;
             currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             Destroy(other.gameObject);
-            bumpPowerupIndicator.gameObject.SetActive(true);
+            PowerUpType type = other.gameObject.GetComponent<PowerUp>().powerUpType;
+
+            if (type == PowerUpType.Pushback)
+            {
+                bumpPowerupIndicator.gameObject.SetActive(true);
+            }
+            else if (type == PowerUpType.Rockets)
+            {
+                shootPowerupIndicator.gameObject.SetActive(true);
+            }
+            else if (type == PowerUpType.Smash)
+            {
+                smashPowerupIndicator.gameObject.SetActive(true);
+            }
 
             if (powerupCountdown != null)
             {
@@ -79,11 +109,46 @@ public class PlayerController : MonoBehaviour
     }
 
 
+
     IEnumerator PowerupCountdownRoutine()
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
         currentPowerUp = PowerUpType.None;
         bumpPowerupIndicator.gameObject.SetActive(false);
+        smashPowerupIndicator.gameObject.SetActive(false);
+        shootPowerupIndicator.gameObject.SetActive(false);
     }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+        //Store the y position before taking off
+        floorY = transform.position.y;
+        //Calculate the amount of time we will go up
+        float jumpTime = Time.time + hangTime;
+        while (Time.time < jumpTime)
+        {
+            //move the player up while still keeping their x velocity.
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+        //Now move the player down
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+        //Cycle through all enemies.
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            //Apply an explosion force that originates from our position.
+            if (enemies[i] != null)
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce,
+                transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+        //We are no longer smashing, so set the boolean to false
+        smashing = false;
+    }
+
 }
