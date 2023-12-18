71bdc6747e95fa67f94e37bf9350bceb4f5aaa8e
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-12-18 16:05:01 +0800

    error project

---
+++ Error Project/Assets/CongratScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+    
+    private List<string> TextToDisplay;
+    
+    private float RotatingSpeed;
+    public float TimeToNextText;
+
+    private int CurrentText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+        
+        RotatingSpeed = 2.0f;
+
+        TextToDisplay = new List<string>();
+        TextToDisplay.Add("Congratulations");
+        TextToDisplay.Add("All Errors Fixed");
+
+        Text.text = TextToDisplay[0];
+        
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToNextText += Time.deltaTime;
+
+        if (TimeToNextText > RotatingSpeed)
+        {
+            TimeToNextText = 0.0f;
+
+            CurrentText++;
+            if (CurrentText >= TextToDisplay.Count)
+            {
+                CurrentText = 0;
+            }
+            Text.text = TextToDisplay[CurrentText];
+        }
+    }
+}
